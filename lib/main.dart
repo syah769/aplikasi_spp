c885d49a7104ba19ac63278ce7d1cec24d8734a2
Syahril Ashraf <bajuhitam400@gmail.com>
2023-06-30 18:58:16 +0800

    laravel API - 11
    install package get_cli and activate it

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,15 @@
-import 'package:aplikasi_spp/my_homepage.dart';
 import 'package:flutter/material.dart';
 
+import 'package:get/get.dart';
+
+import 'app/routes/app_pages.dart';
+
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  runApp(
+    GetMaterialApp(
+      title: "Application",
+      initialRoute: AppPages.INITIAL,
+      getPages: AppPages.routes,
+    ),
+  );
 }
