b20b4901043d978fbfe1b84675de84b92e62652f
Syahril Ashraf <bajuhitam400@gmail.com>
2023-06-30 22:43:54 +0800

    laravel api - 12
    create main screen with widget column

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, sort_child_properties_last
+
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -12,7 +14,7 @@ class HomeView extends GetView<HomeController> {
       backgroundColor: Colors.white,
       appBar: AppBar(
         leading: Container(
-          padding: const EdgeInsets.only(
+          padding: EdgeInsets.only(
             left: 3,
             top: 3,
             bottom: 3,
@@ -23,17 +25,78 @@ class HomeView extends GetView<HomeController> {
         ),
         elevation: 0,
         backgroundColor: Colors.white,
-        title: const Text(
+        title: Text(
           'SPP ONLINE',
           style: TextStyle(
             color: Colors.black,
           ),
         ),
       ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Container(
+            padding: EdgeInsets.only(
+              left: 20,
+              right: 20,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(height: 10),
+                Image.asset("images/study.png"),
+                Text(
+                  "SPP Online",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  "Bayar dan Monitoring \nWang Sekolah Anak Anda dengan Mudah",
+                  style: TextStyle(
+                    color: Colors.black54,
+                    fontFamily: 'Roboto',
+                    fontSize: 15,
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    " L O G I N",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromARGB(255, 7, 14, 142),
+                    fixedSize: Size(Get.width - 70, 40),
+                  ),
+                ),
+                SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    " S I G N U P",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    fixedSize: Size(Get.width - 70, 40),
+                    side: const BorderSide(
+                      width: 2,
+                      color: Color.fromARGB(255, 7, 14, 142),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
