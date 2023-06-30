e649bfe1b18ddf85e1eb5426bdea7a28d311c5da
Syahril Ashraf <bajuhitam400@gmail.com>
2023-06-30 23:06:02 +0800

    laravel api - 13
    refactor code untuk fokus di controller. So, code lebih clean dan structure

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,23 +1,8 @@
+import 'package:aplikasi_spp/app/routes/app_pages.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
-  //TODO: Implement HomeController
-
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
+  void onTapLogin() {
+    Get.toNamed(Routes.LOGIN);
   }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
 }
