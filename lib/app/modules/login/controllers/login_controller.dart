04c9861bf62a99352d506575a886f229a3853a8a
Syahril Ashraf <bajuhitam400@gmail.com>
2023-07-01 09:13:02 +0800

    laravel API - 16
    create login logic at controller

--- lib/app/modules/login/controllers/login_controller.dart
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -1,23 +1,28 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class LoginController extends GetxController {
-  //TODO: Implement LoginController
+  TextEditingController txtEmail = TextEditingController();
+  TextEditingController txtPassword = TextEditingController();
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
+  //create sebuah function/method
+  void auth() {
+    String email = txtEmail.text;
+    String password = txtPassword.text;
+    if (email.isEmpty || password.isEmpty) {
+      Get.snackbar(
+        "Error",
+        "Email dan Password wajib diisi",
+        backgroundColor: Colors.red,
+        colorText: Colors.white,
+      );
+    } else {
+      Get.snackbar(
+        "Success",
+        "Login Berhasil",
+        backgroundColor: Colors.green,
+        colorText: Colors.white,
+      );
+    }
   }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
 }
--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -17,99 +17,103 @@ class LoginView extends GetView<LoginController> {
         foregroundColor: Colors.black,
       ),
       backgroundColor: Colors.white,
-      body: Container(
-        padding: EdgeInsets.all(25),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Assalamualaikum, ",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 30,
-              ),
-            ),
-            SizedBox(height: 10),
-            Text(
-              "Welcome, Please login here",
-              style: TextStyle(
-                color: Colors.black54,
-              ),
-            ),
-            SizedBox(height: 25),
-            TextField(
-              decoration: InputDecoration(
-                labelText: "Email",
-              ),
-            ),
-            SizedBox(height: 10),
-            TextField(
-              decoration: InputDecoration(
-                labelText: "Password",
-              ),
-            ),
-            SizedBox(height: 30),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text(
-                " L O G I N",
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Assalamualaikum, ",
                 style: TextStyle(
-                  color: Colors.white,
                   fontWeight: FontWeight.bold,
+                  fontSize: 30,
                 ),
               ),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color.fromARGB(255, 7, 14, 142),
-                fixedSize: Size(Get.width, 50),
+              SizedBox(height: 10),
+              Text(
+                "Welcome, Please login here",
+                style: TextStyle(
+                  color: Colors.black54,
+                ),
               ),
-            ),
-            SizedBox(height: 30),
-            Row(
-              children: [
-                Expanded(
-                  child: Divider(
-                    height: 2,
-                    color: Colors.black54,
+              SizedBox(height: 25),
+              TextField(
+                controller: controller.txtEmail,
+                decoration: InputDecoration(
+                  labelText: "Email",
+                ),
+              ),
+              SizedBox(height: 10),
+              TextField(
+                controller: controller.txtPassword,
+                decoration: InputDecoration(
+                  labelText: "Password",
+                ),
+              ),
+              SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () => controller.auth(),
+                child: Text(
+                  " L O G I N",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-                Text(
-                  " Or ",
-                  style: TextStyle(color: Colors.black54),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(255, 7, 14, 142),
+                  fixedSize: Size(Get.width, 50),
                 ),
-                Expanded(
-                  child: Divider(
-                    height: 5,
-                    color: Colors.black54,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 20),
-            Center(
-              child: Column(
+              ),
+              SizedBox(height: 30),
+              Row(
                 children: [
-                  Text(
-                    "Login with media social",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
+                  Expanded(
+                    child: Divider(
+                      height: 2,
                       color: Colors.black54,
                     ),
                   ),
-                  SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset("images/logo.png", width: 30),
-                      SizedBox(height: 30),
-                      Image.asset("images/logo.png", width: 30),
-                      SizedBox(height: 30),
-                      Image.asset("images/logo.png", width: 30),
-                    ],
-                  )
+                  Text(
+                    " Or ",
+                    style: TextStyle(color: Colors.black54),
+                  ),
+                  Expanded(
+                    child: Divider(
+                      height: 5,
+                      color: Colors.black54,
+                    ),
+                  ),
                 ],
               ),
-            )
-          ],
+              SizedBox(height: 20),
+              Center(
+                child: Column(
+                  children: [
+                    Text(
+                      "Login with media social",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black54,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset("images/logo.png", width: 30),
+                        SizedBox(height: 30),
+                        Image.asset("images/logo.png", width: 30),
+                        SizedBox(height: 30),
+                        Image.asset("images/logo.png", width: 30),
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
